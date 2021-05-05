c69efa6ab437bd3f69a240aaf7af782c86d2aef8
Charlie Fadness <boy_916@hotmail.com>
2021-05-04 20:59:19 -0700

    functions in quetions 1-5

--- js/app.js
+++ js/app.js
@@ -9,57 +9,66 @@ alert('welcome ' + username + '!');
 alert('Please answer the following questions with yes or no.');
 
 // // //Question 2
-let answerOne = prompt('Do you think I live in Seattle?').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
-  console.log('You are correct! I live in Seattle!');
-  alert('You are correct! I live in Seattle!');
-  scoreBoard++;
-} else if (answerOne === 'no' || answerOne === 'n') {
-  alert('nope');
-} else {
-  alert('can you at least play along?');
+function q2(){
+  let answerOne = prompt('Do you think I live in Seattle?').toLowerCase();
+  if(answerOne === 'yes' || answerOne === 'y') {
+    console.log('You are correct! I live in Seattle!');
+    alert('You are correct! I live in Seattle!');
+    scoreBoard++;
+  } else if (answerOne === 'no' || answerOne === 'n') {
+    alert('nope');
+  } else {
+    alert('can you at least play along?');
+  }
 }
-
+q2();
 
 // // //Question 3
-let codeNow = prompt('Am I a software developer?').toLowerCase();
 
-if(codeNow === 'yes' || answerOne === 'y') {
-  console.log('You are right! Love to code!');
-  alert('You are right! Love to code!');
-  scoreBoard++;
-} else if (codeNow === 'no' || codeNow === 'n') {
-  alert('Nope, not right.');
-} else {
-  alert('Be a good sport.');
-}
+function q3(){
+  let codeNow = prompt('Am I a software developer?').toLowerCase();
+  if(codeNow === 'yes' || answerOne === 'y') {
+    console.log('You are right! Love to code!');
+    alert('You are right! Love to code!');
+    scoreBoard++;
+  } else if (codeNow === 'no' || codeNow === 'n') {
+    alert('Nope, not right.');
+  } else {
+    alert('Be a good sport.');
+  }
+q3();
 
 // // //Question 4
-let workBro = prompt('Do I like anime?').toLowerCase();
-
-if(workBro === 'yes' || workBro === 'y') {
-  console.log('You know it, my favorite would be Naruto.');
-  alert('Yup! Love me some Naruto');
-  scoreBoard++;
-} else if (workBro === 'no' || workBro === 'n') {
-  alert('I mean, very close.');
-} else {
-  alert('ehh, guess you didn'/'t know');
+function q4(){
+  let workBro = prompt('Do I like anime?').toLowerCase();
+  if(workBro === 'yes' || workBro === 'y') {
+    console.log('You know it, my favorite would be Naruto.');
+    alert('Yup! Love me some Naruto');
+    scoreBoard++;
+  } else if (workBro === 'no' || workBro === 'n') {
+    alert('I mean, very close.');
+  } else {
+    alert('ehh, guess you didn'/'t know');
+  }
 }
+q4();
 
 // // //Question 5
-let codeFellow = prompt('Do I study at Code Fellows?');
-
-if(codeFellow === 'yes' || codeFellow === 'y') {
-  console.log('you know it!');
-  alert('Yup, learning the industry secrets!');
-  scoreBoard++;
-} else if (codeFellow === 'no' || codeFellow === 'n') {
-  alert('guess I learned on the fly!');
-} else {
-  alert('You might as well put something in.');
+function q5(){
+  let codeFellow = prompt('Do I study at Code Fellows?');
+  if(codeFellow === 'yes' || codeFellow === 'y') {
+    console.log('you know it!');
+    alert('Yup, learning the industry secrets!');
+    scoreBoard++;
+  } else if (codeFellow === 'no' || codeFellow === 'n') {
+    alert('guess I learned on the fly!');
+  } else {
+    alert('You might as well put something in.');
+  }
 }
+q5();
+
 // alert('Well thanks for answering all my questions ' + username + ' hope you enjoy my page!');
 
 // //Question 6
@@ -112,6 +121,8 @@ if (!monthAnswer) {
   console.log('After incorrect month entered');
   alert('Sorry you did not answer correctly. But the correct answers were december, march and july');
 }
+
+alert(`Your final score is ${scoreBoard}!`);
 
 //Still Struggling with getting the scoreboard to work.
 
